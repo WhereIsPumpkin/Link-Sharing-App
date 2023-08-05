6ce0abf21c69beae92407213425d9ef74455f400
WhereIsPumpkin <sgogr2021@agruni.edu.ge>
2023-08-05 19:51:30 +0400

    feat: add user login controller + route

--- api/src/controllers/UserController.js
+++ api/src/controllers/UserController.js
@@ -29,3 +29,22 @@ export const registerUser = async (req, res) => {
     console.log(err);
   }
 };
+
+export const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+    if (user && (await user.matchPassword(password))) {
+      generateToken(res, user._id);
+      res.json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        userNumber: user.userNumber,
+      });
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
